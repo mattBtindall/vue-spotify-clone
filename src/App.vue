c8e0645c2b47d68ef26174e31b5de056dfc65a9b
mattbtindall <matthewbtindall@gmail.com>
2023-09-15 12:23:58 +0100

    Initial commit
        * Create a basic vue project using vite

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    data: () => ({
+
+    }),
+    components: {
+        HelloWorld
+    },
+}
+</script>
+
+<template>
+<HelloWorld />
+<button >Click me to authorise</button>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<template>
+<header class="header">
+  <h1 class="header__title">
+    Hello
+  </h1>
+</header>
+</template>
+
+<style lang="scss">
+.header {
+  &__title {
+    font-size: 5rem;
+  }
+}
+</style>
